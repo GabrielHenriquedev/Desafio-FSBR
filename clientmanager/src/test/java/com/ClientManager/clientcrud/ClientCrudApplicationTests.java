ce5865c4c3c5221436a795ac347fe06bffa55946
GabrielHenriqueDev <ghss1@discente.ifpe.edu.br>
2024-11-04 20:56:11 -0300

    Criação do projeto, criando a configuração basica do SpringBoot

---
+++ clientmanager/src/test/java/com/ClientManager/clientcrud/ClientCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClientManager.clientcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
