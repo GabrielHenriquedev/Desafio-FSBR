7d7aa9ac707750612492c42fe69246c192cc5347
GabrielHenriqueDev <ghss1@discente.ifpe.edu.br>
2024-11-04 21:06:08 -0300

    Criando a classe de controladores

---
+++ clientmanager/src/main/java/com/ClientManager/clientcrud/controller/ClientController.java
@@ -0,0 +1,50 @@
+package com.ClientManager.clientcrud.controller;
+
+import com.ClientManager.clientcrud.models.ClientModels;
+import com.ClientManager.clientcrud.repository.ClientRepository;
+import com.ClientManager.clientcrud.service.ClientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ClientController {
+
+    // http://localhost:8080/hello
+    @GetMapping("/hello")
+    public String hello(){
+        return "Salve, mundão!";
+    }
+
+    @GetMapping(path = "/clientes")
+    public List<ClientModels> getClient (){
+
+        return clientRepository.findAll();
+    }
+
+    @PostMapping(path = "/clientes")
+    public ResponseEntity<ClientModels> postClient (@RequestBody ClientModels client){
+        ClientModels createClient = clientService.createClient(client);
+        return new ResponseEntity<>(createClient,  HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/clientes/{email}")
+    public ClientModels putClient (@PathVariable String email, @RequestBody ClientModels client){
+        return clientService.updateClient(email, client);
+    }
+
+    @DeleteMapping("/clientes/{email}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteClient(@PathVariable String email){
+        clientService.deleteClient(email);
+    }
+
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private ClientRepository clientRepository;
+}
